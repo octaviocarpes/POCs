2a403553b1b01057ab049cf8526f77a1e09be65a
ilegra0365 <ilegra0365@ilegra0365s-Mac-mini.local>
2018-01-15 11:18:56 -0200

    Finish POC-Springboot

--- POC-Springboot/src/test/java/hello/HelloControllerTest.java
+++ POC-Springboot/src/test/java/hello/HelloControllerTest.java
@@ -28,4 +28,11 @@ public class HelloControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
     }
+
+    @Test
+    public void getKappa() throws Exception{
+        mvc.perform(MockMvcRequestBuilders.get("/kappa").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Kappa!")));
+    }
 }
