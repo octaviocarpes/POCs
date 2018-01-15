992ecb65f68ce7509e0ec3ba5c2fff50564e8a6f
ilegra0365 <ilegra0365@ilegra0365s-Mac-mini.local>
2018-01-15 13:51:17 -0200

    Added POC-Springboot-ServingWebContent

---
+++ POC-Springboot-ServingWebContent/src/main/java/hello/GreetingController.java
@@ -0,0 +1,24 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@Controller
+public class GreetingController {
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+    @RequestMapping("/")
+    public String home(){
+        return "index";
+    }
+
+}
