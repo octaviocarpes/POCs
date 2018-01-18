31bccadfafe5bc20a46e947fd2b1466ad36af5cd
octaviocarpes <octavio.carpes10@gmail.com>
2018-01-18 20:11:19 -0200

    Added POC SPringboot with mockito

---
+++ Back-end/POC-Spinrgboot-Mockito/src/main/java/app/dao/UserRepository.java
@@ -0,0 +1,13 @@
+
+package app.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import app.bo.User;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
